d2e918a0deb5da37c8ab082d2472b0312815af75
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2017-10-06 00:38:54 +0800

    Follow PEP8

--- ml_ids/adaboost-ids.py
+++ ml_ids/adaboost-ids.py
@@ -13,15 +13,15 @@ checker = list()
 switch_number = -1
 
 PROTOCOLS = {
-    pkt.ipv4.ICMP_PROTOCOL : 0,
-    pkt.ipv4.IGMP_PROTOCOL : 1,
-    pkt.ipv6.ICMP6_PROTOCOL : 2,
-    pkt.ipv4.TCP_PROTOCOL : 4,
-    pkt.ipv4.UDP_PROTOCOL : 5
+    pkt.ipv4.ICMP_PROTOCOL: 0,
+    pkt.ipv4.IGMP_PROTOCOL: 1,
+    pkt.ipv6.ICMP6_PROTOCOL: 2,
+    pkt.ipv4.TCP_PROTOCOL: 4,
+    pkt.ipv4.UDP_PROTOCOL: 5
 }
 
-class PacketChecker(object):
 
+class PacketChecker(object):
     def __init__(self, connection):
         self.connection = connection
         connection.addListeners(self)
@@ -40,7 +40,7 @@ class PacketChecker(object):
         log.info('Classifier loaded')
 
         log.info('Switch active')
-        log.info('Switch number:' +  str(self.number))
+        log.info('Switch number:' + str(self.number))
 
     def set_checker(self, enable):
         self.enable_checker = bool(enable)
@@ -73,54 +73,58 @@ class PacketChecker(object):
         self.connection.send(msg)
 
     def _handle_PacketIn(self, event):
-        log.info('IDS#%s Packet In. Checker: %s' % (self.number, str(self.enable_checker)))
+        log.info('IDS#%s Packet In. Checker: %s' % (self.number,
+                                                    str(self.enable_checker)))
 
         if self.enable_checker is True:
             packet = event.parsed
             packet_in = event.ofp
 
             self.count += 1
-            log.info("Switch#" + str(self.number) + " packet# " + str(self.count))
+            log.info("Switch#" + str(self.number) + " packet# " +
+                     str(self.count))
 
             ip = packet.find('ipv4')
             if ip is None:
                 log.info("Switch# " + str(self.number) + " This isn't IP!")
             else:
 
-                log.info("Switch# " + str(self.number) + " Source IP: " +  str(ip.srcip))
+                log.info("Switch# " + str(self.number) + " Source IP: " +
+                         str(ip.srcip))
 
-                #Do nothing if packet came from host
+                # Do nothing if packet came from host
                 if self.attached_host == ip.srcip:
                     return
 
-                #Check if IP is already blocked
+                # Check if IP is already blocked
                 if str(ip.srcip) in self.black_list:
                     log.info("%s BLOCKED!" % str(ip.srcip))
 
-                    #Create openflow message to set block rule
+                    # Create openflow message to set block rule
                     self.set_block_rule(ip)
                     return EventHalt
 
-                #Check if destination port is recorded as a table rule
+                # Check if destination port is recorded as a table rule
                 if packet.dst not in core.switch_pt.mac_to_port:
                     log.info('Skip packet. Not in mac_to_port')
                     return
 
-                #Check and update count of destination port
+                # Check and update count of destination port
                 dst_port = core.switch_pt.mac_to_port[packet.dst]
                 if dst_port in self.destination_port_count_list:
                     self.destination_port_count_list[dst_port] += 1
                 else:
                     self.destination_port_count_list[dst_port] = 1
 
-                #Check and update count of source IP
+                # Check and update count of source IP
                 if ip.srcip in self.source_ip_count_list:
                     self.source_ip_count_list[ip.srcip] += 1
                 else:
                     self.source_ip_count_list[ip.srcip] = 1
 
-                #Generate array for prediction then classify
-                entry = self.generate_prediction_entry(ip, dst_port, packet, packet_in)
+                # Generate array for prediction then classify
+                entry = self.generate_prediction_entry(ip, dst_port, packet,
+                                                       packet_in)
                 pred = self.clf.predict(entry)
 
                 log.info('Classification: %i' % pred)
@@ -130,19 +134,19 @@ class PacketChecker(object):
                     log.info("Added to blacklist: %s" % str(ip.srcip))
                     self.black_list.append(str(ip.srcip))
 
-                    #Create openflow message to set block rule
+                    # Create openflow message to set block rule
                     self.set_block_rule(ip)
 
-                    #Log IP of anomalous host
+                    # Log IP of anomalous host
                     core.IDSMetricLogger.log_blocked_host(ip.srcip)
 
-#Start Component
-def launch():
 
-    def start_switch (event):
+# Start Component
+def launch():
+    def start_switch(event):
         global switch_number
         switch_number += 1
-        log.debug("Controlling %s" % (event.connection,))
+        log.debug("Controlling %s" % (event.connection, ))
         checker.append(PacketChecker(event.connection))
         core.Interactive.variables['checker'] = checker
 
