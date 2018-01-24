3ffde93bcd3cc1cddf98100d9172563f3d93fb1b
antonsuba <antonsuba@outlook.com>
2018-01-24 17:21:28 +0800

    Switch to precision and recall metrics

--- ml_ids/classifier-validation.py
+++ ml_ids/classifier-validation.py
@@ -3,7 +3,8 @@ import os
 import glob
 from collections import defaultdict
 from sklearn.externals import joblib
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
 
 DATA_PATH = '../training_data/IDS2012'
 CLASSIFIER_FILE = './adaboost-ids.pkl'
@@ -49,6 +50,24 @@ for flow in validation_flows:
     flow[2] = port_counts['source'][flow[2]]
     flow[0] = port_counts['destination'][flow[0]]
 
-clf = joblib.load(CLASSIFIER_FILE)
-pred = clf.predict(validation_flows)
-print 'Accuracy:', accuracy_score(labels, pred)
+forest_clf = joblib.load(CLASSIFIER_FILE)
+# pred = clf.predict(validation_flows)
+# print 'Accuracy:', accuracy_score(labels, pred)
+
+forest_pred = cross_val_predict(forest_clf, validation_flows, labels, cv=5)
+
+cnf_mx = confusion_matrix(labels, forest_pred)
+print 'Confusion Matrix:'
+print cnf_mx
+
+precision = precision_score(labels, forest_pred)
+print 'Precision:'
+print precision
+
+recall = recall_score(labels, forest_pred)
+print 'Recall:'
+print recall
+
+f1 = f1_score(labels, forest_pred)
+print 'F1 Score:'
+print f1
