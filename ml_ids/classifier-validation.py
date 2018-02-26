29220ef88318286a4a8da8286bfe39b7ee67199d
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2018-02-26 18:48:46 +0800

    Convert flows to packets

--- ml_ids/classifier-validation.py
+++ ml_ids/classifier-validation.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from sklearn.externals import joblib
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score, f1_score, recall_score, \
-precision_score, confusion_matrix
+    precision_score, confusion_matrix
 
 CONFIG = '../config/config.yml'
 DATA_PATH = 'validation_data'
@@ -37,23 +37,32 @@ def load_validation_set(data_path):
             for item in temp:
                 correctly_ordered_list = list()
                 try:
-                    classifications.append(convert_class(item['label']))
+                    classifications.append(
+                        convert_class(item[features['label']]))
                 except:
                     classifications.append(convert_class(item['class']))
-                correctly_ordered_list.append(item.pop('Dst Pt'))
-                correctly_ordered_list.append(item.pop('Dst IP Addr'))
-                correctly_ordered_list.append(item.pop('Src Pt'))
-                correctly_ordered_list.append(item.pop('Src IP Addr'))
+                correctly_ordered_list.append(
+                    item.pop(features['destination-port']))
+                correctly_ordered_list.append(
+                    item.pop(features['destination-ip']))
+                correctly_ordered_list.append(
+                    item.pop(features['source-port']))
+                correctly_ordered_list.append(item.pop(features['source-ip']))
                 protocol_one_hot = [0, 0, 0, 0, 0, 0]
-                protocol_one_hot[PROTOCOLS[item['Proto']]] = 1
+                protocol_one_hot[PROTOCOLS[item.pop(features['protocol'])]] = 1
                 correctly_ordered_list += protocol_one_hot
-                ip_counts['source'][correctly_ordered_list[3]] += 1
-                ip_counts['destination'][correctly_ordered_list[1]] += 1
-                port_counts['source'][correctly_ordered_list[2]] += 1
-                port_counts['destination'][correctly_ordered_list[0]] += 1
-                dataset.append(correctly_ordered_list)
-                print loading[int(floor(counter % 4))], "\r",
-                counter += 0.0001
+                packet_count = int(item.pop(features['source-packet-count']))
+                ip_counts['source'][correctly_ordered_list[3]] += packet_count
+                ip_counts['destination'][correctly_ordered_list[
+                    1]] += packet_count
+                port_counts['source'][correctly_ordered_list[
+                    2]] += packet_count
+                port_counts['destination'][correctly_ordered_list[
+                    0]] += packet_count
+                for packet in range(packet_count):
+                    dataset.append(correctly_ordered_list[:])
+                    print loading[int(floor(counter % 4))], "\r",
+                    counter += 0.00001
             print str(file), 'read'
     return dataset, classifications
 
@@ -61,9 +70,14 @@ def load_validation_set(data_path):
 def convert_class(x):
     return int(x.lower() != 'normal')
 
+
+with open(os.path.join(DIRNAME, CONFIG), 'r') as config_file:
+    cfg = yaml.load(config_file).get('ml_ids').get('classifier-validation')
+    features = cfg['feature-names']
 ip_counts = {'source': defaultdict(int), 'destination': defaultdict(int)}
 port_counts = {'source': defaultdict(int), 'destination': defaultdict(int)}
-validation_flows, labels = load_validation_set(DATA_PATH)
+validation_flows, labels = load_validation_set(
+    os.path.join(DIRNAME, DATA_PATH, cfg['validation-dataset-folder']))
 
 for flow in validation_flows:
     flow[3] = ip_counts['source'][flow[3]]
@@ -71,8 +85,9 @@ for flow in validation_flows:
     flow[2] = port_counts['source'][flow[2]]
     flow[0] = port_counts['destination'][flow[0]]
 
-clf = joblib.load(CLASSIFIER_FILE)
-# pred = clf.predict(validation_flows)
+clf = joblib.load(
+    os.path.join(DIRNAME, CLASSIFIERS, cfg['classifier'] + '.pkl'))
+print 'Classifier %s loaded' % cfg['classifier'] + '.pkl'
 # print 'Accuracy:', accuracy_score(labels, pred)
 
 pred = cross_val_predict(clf, validation_flows, labels, cv=3, verbose=10)
