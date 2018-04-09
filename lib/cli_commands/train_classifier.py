3c61440ee44fe61dfa209a379370942509153a5d
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2018-04-09 17:35:18 +0800

    Remove unnecessary path inserts

--- lib/cli_commands/train_classifier.py
+++ lib/cli_commands/train_classifier.py
@@ -4,10 +4,8 @@
 import os
 import sys
 import inspect
-
-DIRNAME = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
-sys.path.insert(0, os.path.join(DIRNAME, '../'))
 from ml_ids import ids_classifier
+
 
 class TrainClassifierCommand(object):
     "Command driver for training IDS classifiers"
--- lib/cli_commands/validate_classifier.py
+++ lib/cli_commands/validate_classifier.py
@@ -4,10 +4,8 @@
 import os
 import sys
 import inspect
-
-DIRNAME = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
-sys.path.insert(0, os.path.join(DIRNAME, '../'))
 from ml_ids import classifier_validation
+
 
 class ValidateClassifierCommand(object):
     "Command driver for validating IDS classifiers"
