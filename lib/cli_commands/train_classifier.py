0b2ab2cc474fc3ce853f4095537165006fc44815
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2018-04-09 17:49:44 +0800

    Make training module configurable

--- lib/cli_commands/train_classifier.py
+++ lib/cli_commands/train_classifier.py
@@ -3,8 +3,14 @@
 
 import os
 import sys
+import yaml
 import inspect
-from ml_ids import ids_classifier
+from importlib import import_module
+
+DIRNAME = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+CONFIG = os.path.join(DIRNAME, '../../config/config.yml')
+with open(CONFIG, 'r') as config_file:
+    cfg = yaml.load(config_file).get('cli')
 
 
 class TrainClassifierCommand(object):
@@ -17,4 +23,5 @@ class TrainClassifierCommand(object):
             print 'No available command args for "train"'
             return
 
-        ids_classifier.main()
+        training_module = import_module('ml_ids.%s' % cfg['training-module'])
+        training_module.main()
