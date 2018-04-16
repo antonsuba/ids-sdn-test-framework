58c9994b84593dceb5e3ee9601dc19ae12454c15
Kurt Vincent Bautista <kurt.bautista@outlook.com>
2018-04-16 23:45:56 +0800

    Update testcase 1 pcap file

--- test_cases/test_case_1.py
+++ test_cases/test_case_1.py
@@ -11,7 +11,7 @@ class TestCase1(object):
                  ext_switches, int_routers, ext_routers):
         self._check_dependencies(self.packages, int_hosts[0])
         self._exec_attack(targets, int_hosts, ext_hosts, int_switches,
-                         ext_switches, int_routers, ext_routers)
+                          ext_switches, int_routers, ext_routers)
 
     def _check_dependencies(self, packages, host):
         result = host.cmd('dpkg -l %s' % (' '.join(packages)))
@@ -20,10 +20,10 @@ class TestCase1(object):
             print 'sudo apt-get install %s' % (' '.join(packages))
 
     def _exec_attack(self, targets, int_hosts, ext_hosts, int_switches,
-                    ext_switches, int_routers, ext_routers):
+                     ext_switches, int_routers, ext_routers):
         eth_intf = ext_routers[0].cmd(
             'ifconfig | grep -E "\w+-eth0\s+"').split()[0]
-        pcap_file = 'pcap/test-15jun.pcap'
+        pcap_file = 'pcap/testbed-15jun.pcap'
 
         info(ext_routers[0].cmd('tcpreplay --topspeed -i %s %s' % (eth_intf,
                                                                    pcap_file)))
